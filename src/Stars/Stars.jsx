52ae9e72e3ce9f2681faeb980d8ac38c43d36d4d
Antonin Riviere <antonin.rvr@gmail.com>
2024-12-18 18:57:16 +0100

    :sparkles: simplify stars positions

--- src/Stars/Stars.jsx
+++ src/Stars/Stars.jsx
@@ -4,30 +4,22 @@ import { Points, PointMaterial } from '@react-three/drei'
 export default function Stars() {
   const points = useMemo(() => {
     const positions = []
-    const count = 1500
+    const count = 300
 
     for (let i = 0; i < count; i++) {
-      // radius of the sphere
-      const r = 70 + Math.random() * 200
-
-      // theta is between 0 & π, phi 0 & 2π
-      const u = Math.random()
-      const v = Math.random()
-      const theta = Math.acos(1 - 2 * u)
-      const phi = 2 * Math.PI * v
-
-      const x = r * Math.sin(theta) * Math.cos(phi)
-      const y = r * Math.sin(theta) * Math.sin(phi)
-      const z = r * Math.cos(theta)
+      const x = (Math.random() - 0.5) * 200
+      const y = 8 + Math.random() * 50
+      const z = -50 + Math.random() * -200
 
       positions.push(x, y, z)
     }
+  
     return new Float32Array(positions)
   }, [])
 
   return (
     <Points positions={points}>
-      <PointMaterial transparent color="white" size={0.5} />
+      <PointMaterial transparent color="white" size={0.25} fog={false} />
     </Points>
   )
 }
