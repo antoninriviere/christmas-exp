7f11f6207fa643eedc6b129ef659e9075f0bacbe
Antonin Riviere <antonin.rvr@gmail.com>
2024-12-24 03:29:07 +0100

    :lipstick: Forest > Adjust trees positions

--- src/Forest/Forest.jsx
+++ src/Forest/Forest.jsx
@@ -13,7 +13,7 @@ export default function Forest() {
 
   const trees = useMemo(() => {
     const treesArray = []
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < 65; i++) {
       const isPine = Math.random() < 0.3
 
       const scales = [
@@ -28,11 +28,14 @@ export default function Forest() {
 
       do {
         position = [
-          (Math.random() - 0.5) * 20,
+          (Math.random() - 0.5) * 25,
           yPos,
-          (Math.random() - 0.5) * 20
+          (Math.random() - 0.5) * 25
         ]
-      } while (Math.sqrt(position[0] ** 2 + position[2] ** 2) < 6)
+      } while (
+        Math.sqrt(position[0] ** 2 + position[2] ** 2) < 5 || // circle in center to avoid
+        (position[0] > -5 && position[0] < 5 && position[2] > 0 && position[2] < 12) // zone in front of camera to avoid x -5 to 5, z 0 to 8
+      )
 
       treesArray.push({ isPine, scale, position })
     }
@@ -49,6 +52,11 @@ export default function Forest() {
           position={tree.position}
         />
       ))}
+
+      <Clone object={models.snowPine} position={[2, 0.35, 10]} scale={[3, 3, 3]} />
+      <Clone object={models.pine} position={[-2.5, 0.3, 11]} scale={[4, 4, 4]} />
+      <Clone object={models.pine} position={[-0.65, 0.3, 11.25]} scale={[3, 3, 3]} />
+      <Clone object={models.snowPine} position={[-3.65, 0.3, 10]} scale={[4, 4, 4]} />
     </>
   )
 }
